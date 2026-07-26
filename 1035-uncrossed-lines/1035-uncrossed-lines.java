d78c0d0ad009e46da6e5f95b679cbc59106cb3fd
RITESH KUMAR <164740498+riteshkumar512@users.noreply.github.com>
2026-07-26 10:52:06 +0530

    Time: 6 ms (83.33%), Space: 45.9 MB (64.19%) - LeetHub

---
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -0,0 +1,17 @@
+    class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+		for (int i = 1; i < dp.length; i++) {
+			for (int j = 1; j < dp[0].length; j++) {
+				if (nums1[i - 1] == nums2[j - 1]) {
+					dp[i][j] = 1 + dp[i - 1][j - 1];
+				} else {
+					int f = dp[i - 1][j];
+					int s = dp[i][j - 1];
+					dp[i][j] = Math.max(f, s);
+				}
+			}
+		}
+		return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
